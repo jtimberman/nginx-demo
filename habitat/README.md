8a755d175599d18903873ca225f56d4a14208ccc
jtimberman <joshua@chef.io>
2018-07-18 13:08:50 -0600

    create a package to demonstrate

---
+++ habitat/README.md
@@ -0,0 +1,5 @@
+# Habitat package: nginx-demo
+
+## Description
+
+This is a package for demonstration purposes.
---
+++ habitat/plan.sh
@@ -0,0 +1,14 @@
+pkg_name="nginx-demo"
+pkg_origin="jtimberman"
+pkg_version="0.1.0"
+pkg_maintainer="Joshua Timberman <joshua@chef.io>"
+pkg_deps=(core/nginx)
+pkg_svc_user="root"
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
